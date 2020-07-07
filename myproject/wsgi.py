85a2c8c5c270a1ae8181f4364b84546d349f2efd
rinkeshmakwana <ricks14348@gmail.com>
2020-07-08 00:12:57 +0530

    wsgi updated

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings.development')
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
