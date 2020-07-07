f97f880f63d284752f62f5725309f55c20b5badf
rinkeshmakwana <ricks14348@gmail.com>
2020-07-07 19:23:53 +0530

    dev mode updated

--- myproject/settings/development.py
+++ myproject/settings/development.py
@@ -7,9 +7,9 @@ INSTALLED_APPS += [
     'debug_toolbar',
 ]
 
-MIDDLEWARE += [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
+# MIDDLEWARE += [
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# ]
 
 # DEBUG TOOLBAR SETTINGS
 
